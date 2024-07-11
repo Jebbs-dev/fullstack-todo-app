4b5011e36f3c7bdb9de7660b77dfc26efe907eda
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-11 20:38:04 +0100

    feat: add constants
    Add dummy data to test api calls

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    id: 1,
+    name: "Jack Smith",
+    email: "jacksmith@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Josh Green",
+    email: "jgreen@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Sam Ricks",
+    email: "samricks@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Phoebe Chan",
+    email: "phechan@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Rose Vera",
+    email: "rosevera@gmail.com",
+  },
+];
