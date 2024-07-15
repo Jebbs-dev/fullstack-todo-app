969420fda670bc0784ccace313aa0d921ac8ec8e
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-15 23:31:51 +0100

    feat: add custom router for handling task requests

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import usersRouter from "./users";
+import tasksRouter from "./tasks";
 
 const router: Router = Router();
 
 router.use(usersRouter);
+router.use(tasksRouter);
 
 export default router;
