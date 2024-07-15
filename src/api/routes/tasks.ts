b8ad484105817e1ea724211312e18fba319281da
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-15 23:32:15 +0100

    feat: add tasks route

---
+++ src/api/routes/tasks.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { tasks } from "../../utils/constants";
+import { TaskType } from "../../../types";
+
+const router: Router = Router();
+
+router.get("/api/tasks", (req, res) => {
+  res.send(tasks);
+});
+router.get("/api/tasks/pending", (req, res) => {
+  const task = tasks.find((task) => task.status === "pending");
+  res.send(task);
+});
+
+router.get("/api/tasks/progressing", (req, res) => {
+  const task = tasks.find((task) => task.status === "in progress");
+  res.send(task);
+});
+
+router.get("/api/tasks/completed", (req, res) => {
+  const task = tasks.find((task) => task.status === "completed");
+  res.send(task);
+});
+
+router.get("/api/tasks/:id", (req, res) => {
+  const { id } = req.params;
+
+  const parsedId = parseInt(id);
+
+  const task = tasks.find((task: TaskType) => task.id === parsedId);
+
+  if (!task) {
+    return res.status(404).send("Task not found");
+  }
+
+  res.send(task);
+});
+
+export default router;
