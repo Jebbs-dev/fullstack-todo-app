20373197a108f90da6316a738886acb76119aaac
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-22 23:20:40 +0100

    feat: add virtual for task relation

--- src/schemas/mongoose/user.ts
+++ src/schemas/mongoose/user.ts
@@ -2,10 +2,17 @@ import mongoose, { Schema } from "mongoose";
 
 const UserSchema: Schema = new Schema({
   email: { type: String, required: true, unique: true },
-  name: { type: String, required: true},
+  name: { type: String, required: true },
   password: { type: String, required: true },
   username: { type: String },
   createdAt: { type: Date, default: Date.now() },
+  // tasks: [{type: mongoose.Schema.Types.ObjectId, ref: "Task"}]
 });
+
+UserSchema.virtual('tasks', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'userId'
+})
 
 export const User = mongoose.model("User", UserSchema);
