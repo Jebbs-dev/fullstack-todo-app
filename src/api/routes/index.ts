b5b2e4ac5c9d3918a4acef3265122c0eadb2c2d8
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-22 23:27:21 +0100

    feat: add auth router

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import authRouter from "./auth"
 import usersRouter from "./users";
 import tasksRouter from "./tasks";
 
 const router: Router = Router();
 
+router.use(authRouter);
 router.use(usersRouter);
 router.use(tasksRouter);
 
