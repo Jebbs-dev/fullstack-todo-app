47e77b7ceaf3282c09168df1933848f12f806f55
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-22 23:24:38 +0100

    chore: keep entry point lean
    Moved authentication logic to its own route to keep the entry point lean

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,6 @@ import MongoStore from "connect-mongo";
 import "./strategies/local-strategy";
 import router from "./api/routes";
 
-import { ExtendedSession } from "../types";
-import { User } from "./schemas/mongoose/user";
-
 dotenv.config();
 
 const app: Express = express();
@@ -48,43 +45,7 @@ app.use(passport.session());
 app.use(router);
 
 app.get("/", (req: Request, res: Response) => {
-  // console.log(req.cookies);
-  console.log(req.session);
-  console.log(req.session.id);
-
-  (req.session as ExtendedSession).visited = true;
   res.send("Hello, world!");
-});
-
-app.post(
-  "/api/auth",
-  passport.authenticate("local"),
-  async (req: Request, res: Response) => {
-    const {
-      body: { email, password },
-    } = req;
-
- 
-
-    return res.sendStatus(200);
-  }
-);
-
-app.get("/api/auth/status", (req: Request, res: Response) => {
-  return req.user
-    ? res.status(200).send(req.user)
-    : res.status(401).send({ msg: "Not Authenticated" });
-});
-
-app.post("/api/auth/logout", (req: Request, res: Response) => {
-  if (!req.user) return res.sendStatus(401);
-
-  req.logOut((error) => {
-    if (error) {
-      res.sendStatus(400);
-    }
-    res.sendStatus(200);
-  });
 });
 
 const PORT = process.env.PORT || 3000;
